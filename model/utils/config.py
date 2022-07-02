52daac19863c59ba7e5efa85be1f9ce16cf0c46d
kdrivas <k.rivas@pucp.edu.pe>
2022-07-01 22:13:13 -0500

    Creating constants, function for feature engineering and config

---
+++ model/utils/config.py
@@ -0,0 +1,10 @@
+# Folder config
+RAW_DIR = "raw"
+INTERM_DIR = "interm"
+
+# Data path
+MILK_FILE_NAME = "precio_leche"
+PREP_FILE_NAME = "precipitaciones"
+BANK_FILE_NAME = "banco_central"
+
+MERGED_FILE_NAME = "merge_data"
