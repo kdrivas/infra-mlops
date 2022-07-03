0e4c3f65a46448cfacf73177b9b4c8a12039f5f7
kdrivas <k.rivas@pucp.edu.pe>
2022-07-03 13:12:37 -0500

    Updating library installation

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 from setuptools import setup
 
-with open("README.md", encoding="utf-8") as f:
-    long_description = f.read()
-
 with open("requirements.txt") as f:
     required = f.read().splitlines()
 
@@ -10,7 +7,5 @@ setup(
     name="model",
     version="0.1",
     packages=["model"],
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     install_requires=required,
 )
